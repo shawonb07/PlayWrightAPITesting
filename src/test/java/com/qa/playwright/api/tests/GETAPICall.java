d5f4ea383b660e74fc0ea5e8633bd4b739873d0c
Shawon Biswas <shawonbiswas@Ignatious-MacBook-Pro.local>
2023-04-14 15:19:24 -0500

    Initial Commit

--- src/test/java/com/qa/playwright/api/tests/GETAPICall.java
+++ src/test/java/com/qa/playwright/api/tests/GETAPICall.java
@@ -1,2 +1,29 @@
-package com.qa.playwright.api.tests;public class GETAPICall {
+package com.qa.playwright.api.tests;
+
+import com.microsoft.playwright.APIRequestContext;
+import com.microsoft.playwright.APIResponse;
+import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.APIRequest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class GETAPICall {
+
+    @Test
+    public void getUsersApiTests(){
+
+        Playwright playwright = Playwright.create();
+        APIRequest apiRequest = playwright.request();
+        APIRequestContext apiRequestContext = apiRequest.newContext();
+        APIResponse apiResponse = apiRequestContext.get("https://gorest.co.in/public/v2/users");
+
+        int statusCode = apiResponse.status();
+        System.out.println("response status code: " + statusCode);
+        Assert.assertEquals(statusCode, 200);
+
+        String statusResponseText = apiResponse.statusText();
+        System.out.println(statusResponseText);
+
+        apiResponse.body();
+    }
 }
